46b483c5632372a59cf78d9d7707eeb9ccb37481
suzhetao <suzhetao@gmail.com>
2024-02-06 15:51:08 +0800

    modified:   nat-gateway/locals.tf 	modified:   nat-gateway/resources.tf

--- nat-gateway/locals.tf
+++ nat-gateway/locals.tf
@@ -6,25 +6,70 @@ locals {
       display_name        = s.display_name
     }
   ])
+  vpc_flat = flatten([
+    for s in var.resources[*] : [
+      for t in s.vpc[*] : {
+        display_name = s.resource_manager_resource_group.display_name
+        cidr_block   = t.cidr_block
+        vpc_name     = t.vpc_name
+      }
+    ]
+  ])
+  vswitch_flat = flatten([
+    for s in var.resources[*] : [
+      for t in s.vpc[*] : [
+        for u in t.vswitch : {
+          display_name = s.resource_manager_resource_group.display_name
+          vpc_name     = t.vpc_name
+          vswitch_name = u.vswitch_name
+        }
+      ]
+    ]
+  ])
   nat_gateway_flat = flatten([
     for s in var.resources[*] : [
-      for t in s.nat_gateway[*] : {
-        vpc_name             = t.vpc_name
-        specification        = lookup(t, "specification", "Small")
-        nat_gateway_name     = lookup(t, "nat_gateway_name", null)
-        description          = lookup(t, "description", null)
-        dry_run              = lookup(t, "dry_run", false)
-        force                = lookup(t, "force", false)
-        payment_type         = lookup(t, "payment_type", "PayAsYouGo")
-        period               = lookup(t, "period", null)
-        nat_type             = "Enhanced"
-        vswitch_name         = t.vswitch_name
-        internet_charge_type = "PayByLcu"
-        tags                 = lookup(t, "tags", {})
-        deletion_protection  = lookup(t, "deletion_protection", false)
-        network_type         = lookup(t, "network_type", "internet")
-        eip_bind_mode        = lookup(t, "eip_bind_mode", "MULTI_BINDED")
-      }
+      for t in s.vpc[*] : [
+        for u in t.vswitch[*] : [
+          for v in u.nat_gateway : {
+            display_name                           = s.resource_manager_resource_group.display_name
+            vpc_name                               = t.vpc_name
+            specification                          = lookup(v, "specification", "Small")
+            nat_gateway_name                       = lookup(v, "nat_gateway_name", null)
+            description                            = lookup(v, "description", null)
+            dry_run                                = lookup(v, "dry_run", false)
+            force                                  = lookup(v, "force", false)
+            payment_type                           = lookup(v, "payment_type", "PayAsYouGo")
+            period                                 = lookup(v, "period", null)
+            nat_type                               = "Enhanced"
+            vswitch_name                           = u.vswitch_name
+            internet_charge_type                   = "PayByLcu"
+            tags                                   = lookup(v, "tags", {})
+            deletion_protection                    = lookup(v, "deletion_protection", false)
+            network_type                           = lookup(v, "network_type", "internet")
+            eip_bind_mode                          = lookup(v, "eip_bind_mode", "MULTI_BINDED")
+            eip_activity_id                        = lookup(v, "eip_activity_id", null)
+            eip_address_name                       = lookup(v, "eip_address_name", "eip-${v.nat_gateway_name}")
+            eip_auto_pay                           = lookup(v, "eip_auto_pay", true)
+            eip_bandwidth                          = lookup(v, "eip_bandwidth", 5)
+            eip_deletion_protection                = lookup(v, "eip_deletion_protection", false)
+            eip_description                        = lookup(v, "eip_description", null)
+            eip_high_definition_monitor_log_status = lookup(v, "eip_high_definition_monitor_log_status", "OFF")
+            eip_internet_charge_type               = lookup(v, "eip_internet_charge_type", "PayByTraffic")
+            eip_ip_address                         = lookup(v, "eip_ip_address", null)
+            eip_isp                                = lookup(v, "eip_isp", "BGP")
+            eip_log_project                        = lookup(v, "eip_log_project", null)
+            eip_log_store                          = lookup(v, "eip_log_store", null)
+            eip_payment_type                       = lookup(v, "eip_payment_type", "PayAsYouGo")
+            eip_period                             = lookup(v, "eip_period", null)
+            eip_pricing_cycle                      = lookup(v, "eip_pricing_cycle", null)
+            eip_public_ip_address_pool_id          = lookup(v, "eip_public_ip_address_pool_id", null)
+            eip_security_protection_types          = lookup(v, "eip_security_protection_types", [])
+            eip_tags                               = lookup(v, "eip_tags", {})
+            eip_zone                               = lookup(v, "eip_zone", null)
+            snat_source_cidr                       = lookup(v, "snat_source_cidr", "0.0.0.0/0")
+          }
+        ] if can(u.nat_gateway)
+      ]
     ]
   ])
 }
--- nat-gateway/resources.tf
+++ nat-gateway/resources.tf
@@ -7,7 +7,8 @@ data "alicloud_resource_manager_resource_groups" "resource_manager_resource_grou
 
 # 获取专有网络ID。
 data "alicloud_vpcs" "vpcs" {
-  for_each          = { for s in local.nat_gateway_flat : format("%s", s.vpc_name) => s }
+  for_each          = { for s in local.vpc_flat : format("%s", s.vpc_name) => s }
+  cidr_block        = each.value.cidr_block
   status            = "Available"
   resource_group_id = data.alicloud_resource_manager_resource_groups.resource_manager_resource_groups[each.value.display_name].groups.0.id
   vpc_name          = each.key
@@ -15,7 +16,7 @@ data "alicloud_vpcs" "vpcs" {
 
 # 获取交换机ID。
 data "alicloud_vswitches" "vswitches" {
-  for_each          = { for s in local.nat_gateway_flat : format("%s", s.vswitch_name) => s }
+  for_each          = { for s in local.vswitch_flat : format("%s", s.vswitch_name) => s }
   vswitch_name      = each.key
   status            = "Available"
   vpc_id            = data.alicloud_vpcs.vpcs[each.value.vpc_name].vpcs.0.id
@@ -40,4 +41,45 @@ resource "alicloud_nat_gateway" "nat_gateway" {
   deletion_protection  = each.value.deletion_protection
   network_type         = each.value.network_type
   eip_bind_mode        = each.value.eip_bind_mode
+}
+
+# 添加弹性公网IP。
+resource "alicloud_eip_address" "eip_address" {
+  for_each            = { for s in local.nat_gateway_flat : format("%s", s.eip_address_name) => s if s.network_type == "internet" }
+  activity_id         = each.value.eip_activity_id
+  address_name        = each.value.eip_address_name
+  auto_pay            = each.value.eip_auto_pay
+  bandwidth           = each.value.eip_bandwidth
+  deletion_protection = each.value.eip_deletion_protection
+  description         = each.value.eip_description
+  #high_definition_monitor_log_status = each.value.eip_high_definition_monitor_log_status
+  internet_charge_type = each.value.eip_internet_charge_type
+  ip_address           = each.value.eip_ip_address
+  isp                  = each.value.eip_isp
+  #log_project                        = each.value.eip_log_project
+  #log_store                          = each.value.eip_log_store
+  payment_type              = each.value.eip_payment_type
+  period                    = each.value.eip_period
+  pricing_cycle             = each.value.eip_pricing_cycle
+  public_ip_address_pool_id = each.value.eip_public_ip_address_pool_id
+  resource_group_id         = data.alicloud_resource_manager_resource_groups.resource_manager_resource_groups[each.value.display_name].groups.0.id
+  security_protection_types = each.value.eip_security_protection_types
+  tags                      = merge(var.tags, each.value.eip_tags)
+  zone                      = each.value.eip_zone
+}
+
+# 绑定弹性公网IP。
+resource "alicloud_eip_association" "eip_association" {
+  for_each      = { for s in local.nat_gateway_flat : format("%s", s.eip_address_name) => s if s.network_type == "internet" }
+  allocation_id = alicloud_eip_address.eip_address[each.key].id
+  instance_id   = alicloud_nat_gateway.nat_gateway[each.value.nat_gateway_name].id
+}
+
+# 创建SNAT条目。
+resource "alicloud_snat_entry" "snat_entry" {
+  for_each        = { for s in local.nat_gateway_flat : format("%s", s.nat_gateway_name) => s if s.network_type == "internet" }
+  snat_entry_name = "snat-${each.key}"
+  snat_table_id   = alicloud_nat_gateway.nat_gateway[each.key].snat_table_ids
+  source_cidr     = each.value.snat_source_cidr
+  snat_ip         = alicloud_eip_address.eip_address[each.value.eip_address_name].ip_address
 }
